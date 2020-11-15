572e4cd0450967ef86c633e69613276b712eebb2
AntonUA96 <65168207+AntonUA96@users.noreply.github.com>
2020-11-15 21:45:52 +0200

    edit hw

--- task-2.js
+++ task-2.js
@@ -7,21 +7,27 @@ const ingredients = [
     'Приправы',
   ];
 
-const ul = document.querySelector('ul');
+const ul = document.querySelector('#ingredients');
 
+ingredients.map(e => {
+  const li = document.createElement('li');
+  li.textContent = e;
 
-const li1 = document.createElement('li');
-const li2 = document.createElement('li');
-const li3 = document.createElement('li');
-const li4 = document.createElement('li');
-const li5 = document.createElement('li');
-const li6 = document.createElement('li');
+  ul.prepend(li);
+})
 
-li1.innerText = 'Картошка';
-li2.innerText = 'Грибы';
-li3.innerText = 'Чеснок';
-li4.innerText = 'Помидоры';
-li5.innerText = 'Зелень';
-li6.innerText = 'Приправы';
+// const li1 = document.createElement('li');
+// const li2 = document.createElement('li');
+// const li3 = document.createElement('li');
+// const li4 = document.createElement('li');
+// const li5 = document.createElement('li');
+// const li6 = document.createElement('li');
 
-ul.append(li1, li2, li3, li4, li5, li6);
+// li1.innerText = 'Картошка';
+// li2.innerText = 'Грибы';
+// li3.innerText = 'Чеснок';
+// li4.innerText = 'Помидоры';
+// li5.innerText = 'Зелень';
+// li6.innerText = 'Приправы';
+
+// ul.append(li1, li2, li3, li4, li5, li6);
--- task-3.js
+++ task-3.js
@@ -18,32 +18,53 @@ const images = [
 
   const ul = document.querySelector('ul#gallery');
 
-  const li1 = document.createElement('li');
-  const li2 = document.createElement('li');
-  const li3 = document.createElement('li');
+  images.map(e => {
 
-  const img1 = document.createElement('img');
-  const img2 = document.createElement('img');
-  const img3 = document.createElement('img');
+  const li = document.createElement('li');
+  const img = document.createElement('img');
+  li.appendChild(img);
 
-  li1.appendChild(img1);
-  li2.appendChild(img2);
-  li3.appendChild(img3);
+  img.src = e.url;
+  img.alt = e.alt;
 
-  img1.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-  img2.src = 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-  img3.src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+  ul.prepend(li);
 
-  img1.alt = 'White and Black Long Fur Cat';
-  img1.alt = 'Orange and White Koi Fish Near Yellow Koi Fish';
-  img1.alt = 'Group of Horses Running';
-
-  img1.width = 300;
-  img2.width = 300;
-  img3.width = 300;
+  img.width = 300;
 
   ul.style.display = 'flex';
   ul.style.alignItems = 'center';
   ul.style.justifyContent = 'center';
+  });
+
+
+
+
+  // const li1 = document.createElement('li');
+  // const li2 = document.createElement('li');
+  // const li3 = document.createElement('li');
+
+  // const img1 = document.createElement('img');
+  // const img2 = document.createElement('img');
+  // const img3 = document.createElement('img');
+
+  // li1.appendChild(img1);
+  // li2.appendChild(img2);
+  // li3.appendChild(img3);
+
+  // img1.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+  // img2.src = 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+  // img3.src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+
+  // img1.alt = 'White and Black Long Fur Cat';
+  // img1.alt = 'Orange and White Koi Fish Near Yellow Koi Fish';
+  // img1.alt = 'Group of Horses Running';
+
+  // img1.width = 300;
+  // img2.width = 300;
+  // img3.width = 300;
+
+  // ul.style.display = 'flex';
+  // ul.style.alignItems = 'center';
+  // ul.style.justifyContent = 'center';
   
-  ul.append(li1, li2, li3);
+  // ul.append(li1, li2, li3);
